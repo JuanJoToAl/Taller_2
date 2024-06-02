b7567b314a732aeeb1851a81dfafa08e6fa6d929
JuanJoToAl <jutobara@unal.edu.co>
2024-06-02 18:30:50 -0500

    espacio

--- sucesion.py
+++ sucesion.py
@@ -32,13 +32,16 @@ def calcular_sucesion(suma_aprox: float, termino: int,
         
         `termino`: Número de términos utilizados en la serie de Taylor.
     """
-    
+
     # Se calcula el coseno aproximado hasta el térnimo dado mediante for
     for i in range(int(termino + 1)):
         suma_aprox += ((-1) ** i * (variable ** (2 * i)) 
                        / factorial(2 * i))
+        
     # Diferencia entre valor real y aproximación
     diferencia = resultado - suma_aprox
+
     # Error porcentual relativo
     error = abs(diferencia / resultado) * 100
+    
     return suma_aprox, diferencia, error, termino
