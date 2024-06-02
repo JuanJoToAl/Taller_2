2c4b43b2fb41084ea14ef2fa0e3a063680bd5951
JuanJoToAl <jutobara@unal.edu.co>
2024-06-02 18:25:16 -0500

    cambio

--- reiniciar.py
+++ reiniciar.py
@@ -1,25 +1,21 @@
 def reiniciar_variable(suma_aprox : float, termino : float, error : float) -> tuple:
     """
-    La función restablece los valores de `suma_aprox`, `termino` y `error` 
-    a valores iniciales específicos y los devuelve como una tupla.
-    
+    Esta función reinicia los valores de tres variables: 
+    `suma_aprox`, `termino` y `error`.
+
     Args:
-        suma_aprox (float): Representa la suma aproximada de la serie o
-        cálculo.
-
-        termino (float): El parámetro `termino` en la función `reiniciar_variable` representa un valor
-        flotante que se usa como variable dentro de la función. En este contexto, se restablece a un valor
-        de -1 cuando se llama a la función.
-
-        error (float): El parámetro `error` en la función `reiniciar_variable` parece representar el valor
-        de error asociado con algún cálculo o proceso. En la función, la variable `error` se establece en
-        `Ninguno` cuando se llama a la función para restablecer o reiniciar ciertas variables (`suma_aprox`,
-        `termin
-    
+      suma_aprox (float): Representa la suma aproximada de 
+      la serie de Taylor.
+      
+      termino (float): Representa el último término calculado 
+      en la serie de Taylor.
+      
+      error (float): Representa el error relativo de la aproximación 
+      con la serie de Taylor.
+      
     Returns:
-      La función `reiniciar_variable` devuelve una tupla que contiene los valores de `suma_aprox`,
-    `termino` y `error` después de que se hayan restablecido a valores específicos. Los valores que se
-    devuelven son "0" para "suma_aprox", "-1" para "termino" y "Ninguno" para "error".
+      Tupla con las tres variables reiniciadas: 
+      `(suma_aprox, termino, error)`.
     """
 
     # Se reinician las variables
