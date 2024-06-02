cf3621ff6c3b390bb163fe265b93e54a0fac476f
JuanJoToAl <jutobara@unal.edu.co>
2024-06-02 18:30:13 -0500

    comentario

--- sucesion.py
+++ sucesion.py
@@ -3,11 +3,36 @@ from scipy.special import factorial
 
 def calcular_sucesion(suma_aprox: float, termino: int, 
                      variable: float, resultado: float)  -> tuple:
-    """ 
-    Retorna el valor de la sumatoria hasta el término ingresado 
-    por el usuario, la diferencia con el valor real de la función 
-    coseno y el porcentaje de error de la aproximación. 
     """
+    La función calcula una aproximación del coseno de un número 
+    utilizando la serie de Taylor hasta un término especificado.
+
+    Args:
+        suma_aprox (float): Representa la suma acumulada de la serie de Taylor. 
+
+        termino (int): Representa el número máximo de términos a utilizar 
+        en la serie de Taylor.
+
+        variable (float): Variable para la cual se calcula el coseno 
+        aproximado.
+
+        resultado (float): Representa el valor real del coseno de `variable`. 
+        Se utiliza para calcular el error porcentual relativo.
+
+    Returns:
+    Tupla con los siguientes valores:
+        `suma_aprox`: Aproximación del coseno de `variable` hasta el término
+        `termino`.
+        
+        `diferencia`: Diferencia entre el valor real (`resultado`) y la
+        aproximación (`suma_aprox`).
+        
+        `error`: Error porcentual relativo entre el valor real y la
+        aproximación.
+        
+        `termino`: Número de términos utilizados en la serie de Taylor.
+    """
+    
     # Se calcula el coseno aproximado hasta el térnimo dado mediante for
     for i in range(int(termino + 1)):
         suma_aprox += ((-1) ** i * (variable ** (2 * i)) 
